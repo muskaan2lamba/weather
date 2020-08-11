d57375533f9b1230c6c9532883f07f9ef7899f3d
Muskan <muskaan2lamba@gmail.com>
2020-08-12 02:32:26 +0530

    Updates

---
+++ precache-manifest.6f4802830edc4166c72cf6d72f55fed4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd4d287660c5820a2b8287e3faf5854d",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "a48b03d5b24d4a532405",
+    "url": "/weather/static/css/main.608fe13d.chunk.css"
+  },
+  {
+    "revision": "b318303fe207547957eb",
+    "url": "/weather/static/js/2.a1b500d9.chunk.js"
+  },
+  {
+    "revision": "1e085576d753cefdd7bf6028c59ec57a",
+    "url": "/weather/static/js/2.a1b500d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a48b03d5b24d4a532405",
+    "url": "/weather/static/js/main.354bb264.chunk.js"
+  },
+  {
+    "revision": "e741918522270997b492",
+    "url": "/weather/static/js/runtime-main.3b601d73.js"
+  }
+]);
